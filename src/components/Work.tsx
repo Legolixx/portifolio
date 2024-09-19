309e42809bde9ed8c0390e12c8ad9c1184398239
PrimoPhone <primophone69@gmail.com>
2024-09-19 14:53:59 -0300

    projects section finished

---
+++ src/components/Work.tsx
@@ -0,0 +1,99 @@
+"use client";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
+import ProjectCard from "@/components/ProjectCard";
+
+interface DataProject {
+  image: string;
+  category: string;
+  name: string;
+  description: string;
+  link: string;
+  github: string;
+}
+
+const projectData = [
+  {
+    image: "/work/3.png",
+    category: "react js",
+    name: "Nexa Website",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur ",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/4.png",
+    category: "react js",
+    name: "Nexa Website",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur ",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/2.png",
+    category: "next js",
+    name: "Nexa Website",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur ",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/1.png",
+    category: "next js",
+    name: "Nexa Website",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur ",
+    link: "/",
+    github: "/",
+  },
+];
+
+const Work = () => {
+  return (
+    <section className="relative mb-12 xl:mb-48">
+      <div className="container mx-auto">
+        <div className="max-w-[400px] mx-auto xl:mx-0 text-center xl:text-left mb-12 xl:h-[400px] flex flex-col justify-center items-center xl:items-start">
+          <h2 className="section-title mb-4">Latest Projects</h2>
+          <p className="subtitle mb-8">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi
+            excepturi placeat quidem, at voluptatibus voluptatem consequatur.
+          </p>
+          <Link href="/projects">
+            <Button>All projects</Button>
+          </Link>
+        </div>
+        <div className="xl:max-w-[1000px] xl:absolute right-0 top-0">
+          <Swiper
+            className="h-[480px]"
+            slidesPerView={1}
+            breakpoints={{
+              640: {
+                slidesPerView: 2,
+              },
+            }}
+            spaceBetween={30}
+            modules={[Pagination]}
+            pagination={{ clickable: true }}
+          >
+            {projectData.slice(0, 4).map((project: DataProject, index) => {
+              return (
+                <SwiperSlide key={index}>
+                 <ProjectCard project={project} />
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Work;
