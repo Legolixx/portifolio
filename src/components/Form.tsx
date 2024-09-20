98977a6c5218e1960b6be75e6cc668430d0dc310
PrimoPhone <primophone69@gmail.com>
2024-09-20 11:40:57 -0300

    frontend done

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,32 @@
+/* eslint-disable react/no-unescaped-entities */
+"use client";
 
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { User, MailIcon, ArrowRightIcon, MessageSquare } from "lucide-react";
 
 const Form = () => {
   return (
-    <div>Form</div>
-  )
-}
+    <form className="flex flex-col gap-y-4">
+      <div className="relative flex items-center">
+        <Input type="name" id="name" placeholder="Name" />
+        <User className="absolute right-6" size={20} />
+      </div>
+      <div className="relative flex items-center">
+        <Input type="email" id="email" placeholder="Email" />
+        <MailIcon className="absolute right-6" size={20} />
+      </div>
+      <div className="relative flex items-center">
+        <Textarea id="message" placeholder="Type Your Message Here." />
+        <MessageSquare className="absolute top-4 right-6" size={20} />
+      </div>
+      <Button className="flex items-center gap-x-2 max-w-[166px]">
+        Let's Talk
+        <ArrowRightIcon size={20} />
+      </Button>
+    </form>
+  );
+};
 
-export default Form
+export default Form;
